cdf99f1496394097c5a4d39685b1f8176977207b
Joseph Marques <josephmarques@cmail.carleton.ca>
2026-04-04 00:41:39 -0400

    Update Email to send

---
+++ send_email.py
@@ -0,0 +1,46 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+load_dotenv()
+
+client = OpenAI(os.getenv("API_KEY"))
+
+response = client.responses.create(
+    model="gpt-5.4-mini",
+    input="Write a short bedtime story about a unicorn."
+)
+
+# Example usage
+body = response.output_text
+
+print(body)
+
+# Your email credentials
+EMAIL = "marquesjoseph03@gmail.com"
+PASSWORD = "adie tfou zyxd yxuv"
+
+# Receiver
+to_email = "josephmarques@cmail.carleton.ca"
+
+# Create the email
+msg = MIMEMultipart()
+msg["From"] = EMAIL
+msg["To"] = to_email
+msg["Subject"] = "Hello from my script"
+
+msg.attach(MIMEText(body, "plain"))
+
+# Send the email
+try:
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()  # Secure connection
+    server.login(EMAIL, PASSWORD)
+    server.send_message(msg)
+    server.quit()
+    print("Email sent successfully!")
+except Exception as e:
+    print("Error:", e)
